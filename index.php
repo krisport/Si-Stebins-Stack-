df57332bf70bafa9fee851bd49282ef7a39be9ff
krisport <58785013+krisport@users.noreply.github.com>
2020-02-14 18:04:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -32,8 +32,7 @@ session_start();
 			<option value =  "10D KC 3H 6S 9D QC 2H 5S 8D JC AH 4S 7D 10C KH 3S 6D 9C QH 2S 5D 8C JH">10D KC 3H 6S 9D QC 2H 5S 8D JC AH 4S 7D 10C KH 3S 6D 9C QH 2S 5D 8C JH</option>
 			<option value = "6S 9D QC 2D 5C">6S 9D QC 2D 5C</option>
 			<option value = "7C 10H KS 3D 6C 9H QS 2D 5C 8H JS AD 4C 7H 10S KD 3C 6H">7C 10H KS 3D 6C 9H QS 2D 5C 8H JS AD 4C 7H 10S KD 3C 6H</option>
-		</select>
-		<!-- <textarea name="random"></textarea> -->
+	     </select>
 		<input type="submit" name="submit" value="Check the deck">
 	</form>
 <?php
